0eb639179b1a8a19358668ab286ca303069dc455
Sergio Villazon <sergiovillazon@gmail.com>
2018-12-03 19:08:58 -0800

    Removed the filter page

--- js/remove.js
+++ js/remove.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     text = "corporate event";
   }
 
+
   // edit header text
   var header = document.getElementById('book');
   header.innerHTML = "Book these spaces for your " + text.toLowerCase() + "!";
@@ -17,40 +18,51 @@ $(document).ready(function() {
   var arIndex = JSON.parse(localStorage.getItem('arIndex'));
   var indexes = JSON.parse(localStorage.getItem('indexes'));
 
-  // all rooms that match our event become our filterRooms
-  allRooms = ar[arIndex.indexOf(text.toLowerCase())];
-  allIndexes = indexes[arIndex.indexOf(text.toLowerCase())];
+  // get list of rooms that match our event
+  var filterRooms = ar[arIndex.indexOf(text.toLowerCase())];
+  var filterIndexes = indexes[arIndex.indexOf(text.toLowerCase())];
 
-  // Get rooms that match filters, and their indexes
-  var filterRooms = JSON.parse(localStorage.getItem('filterRooms'));
-  var filterIndexes = JSON.parse(localStorage.getItem('filterIndexes'));
+  // set them into local storage
+  localStorage.setItem('filterRooms',JSON.stringify(filterRooms));
+  localStorage.setItem('filterIndexes',JSON.stringify(filterIndexes));
 
-  //if no room match indexes
-  if(filterRooms == null){
 
-    // all rooms that match our event become our filterRooms
-    filterRooms = allRooms;
-    filterIndexes = allIndexes;
-
-  }
+  var results =[];
 
   // Arrays to change filters
   var locString = [];
   var locArray = [];
 
+  // get filters
+  var filters = JSON.parse(localStorage.getItem('filters'));
+  if(filters == null){
+    filters = [];
+  }
   // Get locations that match filters
-  for(var i = 0; i < allRooms.length; i++ ){
-    var roomLocation = allRooms[i]['location'].replace(" ","").toLowerCase();
+  for(var i = 0; i < filterRooms.length; i++ ){
+
+    // change locations to lowercase, delete spaces
+    var roomLocation = filterRooms[i]['location'].replace(/\s/g,'').toLowerCase();
+    var filterLocation = filters[0]['location'].replace(/\s/g,'').toLowerCase();
 
     // populate locations of rooms for filters
     if(locArray.indexOf(roomLocation) == -1){
-      locString.push(allRooms[i]['location']);
+      locString.push(filterRooms[i]['location']);
       locArray.push(roomLocation);
     }
 
-  }
+    // see if room location and filter location match
+    if(roomLocation === filterLocation){
+      var max = parseInt(filters[0]['maxCost'].replace(/\,/g,''));
+      var roomCost = parseInt(filterRooms[i]['price'].replace(/\,/g,''));
 
-  var filters = JSON.parse(localStorage.getItem('filters'));
+      var capacity = parseInt(filters[0]['guest']);
+      if( roomCost <= max){
+        if(capacity <= parseInt(filterRooms[i]['capacity']))
+        results.push(availableRooms[i]);
+      }
+    }
+  }
 
   // updating fliters
   if(filters != null){
@@ -77,57 +89,59 @@ $(document).ready(function() {
     parentDiv.append(curHtml);
   }
 
-  // if the size of our filter rooms is not zero
-  if(filterRooms.length != 0){
+  if(results.length != 0){
     var source   = $("#results-template").html();
     var template = Handlebars.compile(source);
     var parentDiv = $("#first-row");
 
     for (var i = 0; i < 4; i++) {
-      if( i < filterRooms.length){
-        var curData = filterRooms[i];
+      if( i < results.length){
+        var curData = results[i];
         var curHtml = template(curData);
         parentDiv.append(curHtml);
       }
     }
     var parentDiv = $("#second-row");
 
-    for (var i = 4; i < filterRooms.length; i++) {
-      var curData = filterRooms[i];
+    for (var i = 4; i < results.length; i++) {
+      var curData = results[i];
       var curHtml = template(curData);
       parentDiv.append(curHtml);
     }
 
   } else {
-
-    // all rooms that match our event become our filterRooms
-    filterRooms = allRooms;
-    filterIndexes = allIndexes;
-
-    // get template stuff
     var source   = $("#results-template").html();
     var template = Handlebars.compile(source);
     var parentDiv = $('#noResults');
+    var trendingRooms = JSON.parse(localStorage.getItem('filterRooms'));
 
-    for (var i = 0; i < filterRooms.length; i++) {
-      var curData = filterRooms[i];
+    if(trendingRooms == null ){
+      trendingRooms = [];
+    }
+    //var indexes = ['bridge','alcatraz','pier','pc'];
+    for (var i = 0; i < trendingRooms.length; i++) {
+      var curData = trendingRooms[i];
       var curHtml = template(curData);
       parentDiv.append(curHtml);
     }
 
     document.getElementById('first-row').style.display = 'none';
     document.getElementById('second-row').style.display = 'none';
-    document.getElementById('noResults').style.display = 'inline-block';
+    if(filters != null){
+      document.getElementById('noResults').style.display = 'inline-block';
+
+    }
   }
 });
 
 function search(){
-  var loc = document.getElementById('location').innerHTML;
+  var loc = chosenLocation;
   var date = $('#date').val();
   var hours =$('#hours').val();
   var guest = $('#guest').val();
   var min = $('#minCost').val();
   var max = $('#maxCost').val();
+
 
   /* add a default values */
   if(date === ""){
@@ -142,9 +156,6 @@ function search(){
   if(min === ""){
     min = "0";
   }
-  if(max === "" ){
-    max = "1000";
-  }
 
   var filters = [
     {'location':loc, 'date':date, 'hours':hours, 'guest':guest, 'minCost':min, 'maxCost':max}
@@ -152,51 +163,6 @@ function search(){
 
   localStorage.setItem('filters', JSON.stringify(filters));
 
-  // Changing a header
-  var text = localStorage.getItem('event');
-
-  //small change for text
-  if(text === "Corporate"){
-    text = "corporate event";
-  }
-
-  // get list of all rooms and indexes
-  var ar = JSON.parse(localStorage.getItem('availableRooms'));
-  var arIndex = JSON.parse(localStorage.getItem('arIndex'));
-  var indexes = JSON.parse(localStorage.getItem('indexes'));
-
-  // all rooms that match our event become our filterRooms
-  filterRooms = ar[arIndex.indexOf(text.toLowerCase())];
-  filterIndexes = indexes[arIndex.indexOf(text.toLowerCase())];
-
-  // figure out results
-  var results =[];
-  var resultsIndex =[];
-
-  // Get locations that match filters
-  for(var i = 0; i < filterRooms.length; i++ ){
-
-    // change locations to lowercase, delete spaces
-    var roomLocation = filterRooms[i]['location'].replace(/\s/g,'').toLowerCase();
-    var filterLocation = filters[0]['location'].replace(/\s/g,'').toLowerCase();
-
-    // see if room location and filter location match
-    if(roomLocation === filterLocation){
-      var max = parseInt(filters[0]['maxCost'].replace(/\,/g,''));
-      var roomCost = parseInt(filterRooms[i]['price'].replace(/\,/g,''));
-
-      var capacity = parseInt(filters[0]['guest']);
-      if( roomCost <= max){
-        if(capacity <= parseInt(filterRooms[i]['capacity'])){
-          results.push(filterRooms[i]);
-          resultsIndex.push(filterRooms[i]['id']);
-        }
-
-      }
-    }
-  }
-
-  localStorage.setItem('filterRooms',JSON.stringify(results));
   window.location.href='./search.html';
 }
 
@@ -204,6 +170,7 @@ var chosenLocation;
 
 // Drop down menu function
 function dropdown(){
+  console.log('toggling');
   document.getElementById("options").classList.toggle("show");
 }
 
@@ -211,11 +178,4 @@ function chosen(id){
   chosenLocation = id.replace("-"," ");
   document.getElementById('location').innerHTML = id.replace("-"," ");
   document.getElementById("options").classList.toggle("show");
-}
-
-function clearFilters(){
-  console.log('hello');
-  localStorage.setItem('filters',null);
-  localStorage.setItem('filterRooms',null);
-  window.location.href='./search.html';
 }
